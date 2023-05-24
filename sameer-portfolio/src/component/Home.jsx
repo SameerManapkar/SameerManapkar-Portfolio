47e63f0d91372dbb5e5b9000f0a989554203253b
Sameer <sameermanapkar24@gmail.comm>
2023-05-24 13:14:46 +0530

    final touch up

--- sameer-portfolio/src/component/Home.jsx
+++ sameer-portfolio/src/component/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 mt-8 "
+      className=" w-full h-screen bg-gradient-to-b from-black via-black to-gray-800 mt-8 pb-20 pt-10 "
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
--- sameer-portfolio/src/component/NavBar.jsx
+++ sameer-portfolio/src/component/NavBar.jsx
@@ -29,7 +29,7 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20  text-white bg-black fixed top-0 mb-10 px-4 ">
+    <div className="flex justify-between items-center w-full h-20  text-white bg-black fixed top-0 mb-20 px-4 ">
       <div>
         <h1 className="text-5xl font-signature ml-2">Sameer</h1>
       </div>
